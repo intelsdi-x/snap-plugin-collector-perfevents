4545a0f425ad5fce0262aba704794859b4165c18
Andrzej Kuriata <kuriata.andrzej.intc@gmail.com>
2015-11-25 09:44:03 +0100

    Update perfevents_test.go

--- perfevents/perfevents_test.go
+++ perfevents/perfevents_test.go
@@ -24,8 +24,8 @@ package perfevents
 import (
 	"testing"
 
-	"github.com/intelsdi-x/pulse/control/plugin"
-	"github.com/intelsdi-x/pulse/control/plugin/cpolicy"
+	"github.com/intelsdi-x/snap/control/plugin"
+	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
