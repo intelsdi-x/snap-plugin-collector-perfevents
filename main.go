be1bb4b5855a77c9f00db43523d621aef7f98658
Andrzej Kuriata <kuriata.andrzej.intc@gmail.com>
2015-11-25 09:40:02 +0100

    Update main.go

--- main.go
+++ main.go
@@ -22,8 +22,8 @@ package main
 import (
 	"os"
 
-	"github.com/intelsdi-x/pulse-plugin-collector-perfevents/perfevents"
-	"github.com/intelsdi-x/pulse/control/plugin"
+	"github.com/intelsdi-x/snap-plugin-collector-perfevents/perfevents"
+	"github.com/intelsdi-x/snap/control/plugin"
 )
 
 // plugin bootstrap
